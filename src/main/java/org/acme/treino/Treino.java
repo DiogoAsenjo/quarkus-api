56f803d61c8a9583bc07d8b0f1f6d82bcfcacbda
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-18 12:46:16 -0300

    Finalizando o CRUD

--- src/main/java/org/acme/treino/Treino.java
+++ src/main/java/org/acme/treino/Treino.java
@@ -1,21 +1,69 @@
 package org.acme.treino;
 
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import jakarta.json.bind.annotation.JsonbCreator;
 
 public class Treino extends PanacheMongoEntity {
     //ATRIBUTOS
+//    private
     String data;
-    String tempoGasto;
-    double distanciaPercorrida;
-    double velocidadeMaxima;
-    double velocidadeMedia;
+    private String tempoGasto;
+    private double distanciaPercorrida;
+    private double velocidadeMaxima;
+    private double velocidadeMedia;
 
     //CONSTRUTOR
+    public Treino() {
+
+    }
+
     public Treino(String data, String tempoGasto, double distanciaPercorrida, double velocidadeMaxima, double velocidadeMedia) {
         this.data = data;
         this.tempoGasto = tempoGasto;
         this.distanciaPercorrida = distanciaPercorrida;
         this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeMedia = velocidadeMedia;
+    }
+
+    //GETTERS E SETTERS
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTempoGasto() {
+        return tempoGasto;
+    }
+
+    public void setTempoGasto(String tempoGasto) {
+        this.tempoGasto = tempoGasto;
+    }
+
+    public double getDistanciaPercorrida() {
+        return distanciaPercorrida;
+    }
+
+    public void setDistanciaPercorrida(double distanciaPercorrida) {
+        this.distanciaPercorrida = distanciaPercorrida;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public double getVelocidadeMedia() {
+        return velocidadeMedia;
+    }
+
+    public void setVelocidadeMedia(double velocidadeMedia) {
         this.velocidadeMedia = velocidadeMedia;
     }
 }
