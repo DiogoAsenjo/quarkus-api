3763142244fd2637fb21855ee568ae1c338083eb
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-19 16:24:48 -0300

    Configurando o swagger

--- src/main/java/br/com/treinos/controller/TreinoController.java
+++ src/main/java/br/com/treinos/controller/TreinoController.java
@@ -6,11 +6,13 @@ import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
-import java.net.URI;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import java.net.URISyntaxException;
-import java.util.List;
 
 @Path("/treinos")
+@Tag(name = "Treinos", description = "Rotas relacionadas aos treinos")
 public class TreinoController {
 
     @Inject
@@ -18,34 +20,40 @@ public class TreinoController {
 
     @GET
     @Path("/{id}")
+    @Operation(summary = "Busca um treino pelo id")
     public Response get(@PathParam("id") String id) {
        return repository.mostrarTreino(id);
     }
 
     @GET
+    @Operation(summary = "Busca todos os treinos")
     public Response get() {
         return repository.mostrarTodosOsTreinos();
     }
 
     @GET
     @Path("/order/{campo}")
+    @Operation(summary = "Ordena os treinos por um campo", description = "Ordena os treinos para saber os melhores de acordo com os seguintes campos: velocidadeMaxima, velocidadeMedia ou distanciaPercorrida")
     public Response search(@PathParam("campo") String campo) {
         return repository.ordenar(campo);
     }
 
     @POST
+    @Operation(summary = "Cria um novo treino")
     public Response create(@Valid Treino treino) throws URISyntaxException {
         return repository.adicionarTreino(treino);
     }
 
     @PUT
     @Path("/{id}")
+    @Operation(summary = "Altera os dados de um treino", description = "Altera os dados de um treino existente de acordo com o id dele")
     public Response update(@PathParam("id") String id, @Valid Treino treino) {
         return repository.atualizarTreino(id, treino);
     }
 
     @DELETE
     @Path("/{id}")
+    @Operation(summary = "Delete um treino", description = "Delete um treino de acordo com o id dele")
     public Response delete(@PathParam("id") String id) {
         return repository.excluirTreino(id);
     }
